fa3f4db53a05c90434cd622fb2dd35ee153d43e8
qowldud <bjy6631@daum.net>
2024-03-12 16:13:39 +0900

    그랩마켓 서버 추가

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const models = require('./models');
+const port = 8080;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/products",(req,res) => {
+    models.Product.findAll({
+        order : [['createdAt','DESC']],
+        attributes : [
+            'id',
+            'name',
+            'price',
+            'createdAt',
+            'seller'
+        ]
+    }).then((result)=>{
+        console.log("PRODUCTS : ", result);
+        res.send({
+            products : result
+        })
+    }).catch((error)=>{
+        console.error(error);
+        res.send("에러 발생");
+    })
+});
+
+app.post("/products",(req,res) => {
+    const body = req.body;
+    const {name, description, price, seller} = body;
+    if(!name || !description || !price || !seller){
+        res.send("모든 필드를 입력해주세요.");
+    }
+    models.Product.create({
+        name: name, 
+        description: description, 
+        price: price,
+        seller: seller
+    }).then((result) =>{
+        console.log("상품 생성 결과 : ",result)
+        res.send({
+            result,
+        })
+    }).catch((error)=>{
+        console.error(error);
+        res.send("상품 업로드에 문제가 발생했습니다.")
+    })
+});
+
+app.get("/products/:id", (req,res) => {
+    const params = req.params;
+    const {id,eventId} = params;
+    models.Product.findOne({
+        where :{
+            id : id
+        }
+    }).then((result)=>{
+        console.log("PRODUCT : ", result);
+        res.send({
+            product : result
+        })
+    }).catch((error)=>{
+        res.send("상품 조회에 에러가 발생했습니다.");
+    })
+})
+
+app.listen(port, ()=>{
+    console.log('그랩의 쇼핑몰 서버가 돌아가고 있습니다.');
+    models.sequelize.sync().then(()=>{
+        console.log('DB 연결 성공');
+    }).catch((err)=>{
+        console.err(err);
+        console.log('DB 연결 에러');
+        process.exit();
+    })
+})
